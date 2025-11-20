7b47b71f1f75bc70b1d5aa0b64d2efdfb2448cf9
Sigma98 <alsalilee@gmail.com>
2025-11-20 11:20:21 +0200

    Car entity created

--- CarSalesSystem/obj/Debug/net8.0/CarSalesSystem.AssemblyInfo.cs
+++ CarSalesSystem/obj/Debug/net8.0/CarSalesSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarSalesSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db93b5cc9632e6ace8a26eae8b31c4df2e0e65e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarSalesSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarSalesSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
