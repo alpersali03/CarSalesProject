cb78f5ec069b12d8f11c3489450f21ecd5dd6ec5
Sigma98 <alsalilee@gmail.com>
2025-11-20 11:24:10 +0200

    Category entity created

--- CarSalesSystem/Data/Model/Car.cs
+++ CarSalesSystem/Data/Model/Car.cs
@@ -1,6 +1,93 @@
-﻿namespace CarSalesSystem.Data.Model
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarSalesSystem.Data.Model
 {
     public class Car
     {
+        /// <summary>
+        /// car id 
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// car brand 
+        /// </summary>
+        [Required]
+        public string Brand { get; set; } = null!;
+
+        /// <summary>
+        /// car model 
+        /// </summary>
+        [Required]
+        public string Model { get; set; } = null!;
+
+        /// <summary>
+        /// desctription of the car  
+        /// </summary>
+        [Required]
+        public string Description { get; set; } = null!;
+
+        /// <summary>
+        /// photo of the car 
+        /// </summary>
+        [Required]
+        public byte[] CarPhoto { get; set; } = null!;
+
+        /// <summary>
+        /// when the car is made
+        /// </summary>
+
+        public int Year { get; set; }
+
+        /// <summary>
+        /// if the car is public
+        /// </summary>
+        public bool IsPublic { get; set; }
+
+        /// <summary>
+        /// if the car is booked
+        /// </summary>
+        public bool IsBooked { get; set; } = false;
+
+        /// <summary>
+        /// price for 1 day for the car 
+        /// </summary>
+
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// foreign key 
+        /// </summary>  
+        public int CategoryId { get; set; }
+
+        /// <summary>
+        /// foreign key
+        /// </summary> 
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        /// <summary>
+        /// dealer of the car 
+        /// </summary>  
+        public int DealerId { get; set; }
+
+        /// <summary>
+        /// dealer of the car 
+        /// </summary>  
+        [ForeignKey("DealerId")]
+        public Dealer Dealer { get; set; } = null!;
+
+        /// <summary>
+        /// if car is booked this is the id of booking 
+        /// </summary>
+        public int? BookingId { get; set; }
+
+        [Required]
+        public string Country { get; set; } = null!;
+
+        [Required]
+        public string City { get; set; } = null!;
     }
 }
---
+++ CarSalesSystem/Data/Model/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSalesSystem.Data.Model
+{
+    public class Category
+    {
+        /// <summary>
+        /// category id 
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// name of the category 
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// relation of the tables 
+        /// </summary>
+        public IEnumerable<Car>? Cars { get; init; } = new List<Car>();
+    }
+}
