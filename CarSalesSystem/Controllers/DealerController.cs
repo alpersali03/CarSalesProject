2b22d870dd8a8057314f0f6b3547b3af579cec53
Sigma98 <alsalilee@gmail.com>
2025-11-25 11:28:13 +0200

    Dealer crud operations are updated

--- CarSalesSystem/Controllers/DealerController.cs
+++ CarSalesSystem/Controllers/DealerController.cs
@@ -1,12 +1,112 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarSalesSystem.Data;
+using CarSalesSystem.Data.Model;
+using CarSalesSystem.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarSalesSystem.Controllers
 {
     public class DealerController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public DealerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var dealer = _context.Dealers.Include(d=>d.Cars).Select(d=> new DealerDto
+            {
+                Id = d.Id, 
+                Name = d.Name,
+                CompanyName = d.CompanyName,
+                PhoneNumber = d.PhoneNumber,
+                UserId = d.UserId,
+            }).ToList();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new DealerDto());
+        }
+        [HttpPost]
+        public IActionResult Add(DealerDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(dto);
+            }
+            var delaer = new Dealer
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                CompanyName = dto.CompanyName,
+                PhoneNumber = dto.PhoneNumber,
+                UserId = dto.UserId,
+            };
+            _context.Dealers.Add(delaer);
+            _context.SaveChanges();
+            return RedirectToAction("GetAll");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var dealer = _context.Dealers.FirstOrDefault(d => d.Id == id);
+            if (dealer == null)
+            {
+                return NotFound();
+            }
+            var dto = new DealerDto
+            {
+                Id = dealer.Id,
+                Name = dealer.Name,
+                CompanyName = dealer.CompanyName,
+                PhoneNumber = dealer.PhoneNumber,
+                UserId = dealer.UserId,
+            };
+            return View(dto);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, DealerDto dealerDto)
+        {
+            if (!ModelState.IsValid)
+                return View(dealerDto);
+
+            var dealer = _context.Dealers.FirstOrDefault(d => d.Id == id);
+            if (dealer == null)
+            {
+                return NotFound();
+            }
+            var dto = new DealerDto
+            {
+                Id = dealer.Id,
+                Name = dealer.Name,
+                CompanyName = dealer.CompanyName,
+                PhoneNumber = dealer.PhoneNumber,
+                UserId = dealer.UserId,
+            };
+            _context.SaveChanges();
+            return RedirectToAction("GetAll");
+        }
+        [HttpPost]
+        public IActionResult Details(int id)
+        {
+            var dealer = _context.Dealers.Include(d=>d.Cars).FirstOrDefault(d=> d.Id == id);
+            if(dealer == null)
+            {
+                return NotFound();
+            }
+            return View(dealer);
+        }
+        
     }
 }
