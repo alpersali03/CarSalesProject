d42294762521d9f940512eb04ffe0211007e2893
Sigma98 <alsalilee@gmail.com>
2025-11-25 10:15:10 +0200

    Car controller updated

--- CarSalesSystem/Controllers/CarController.cs
+++ CarSalesSystem/Controllers/CarController.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using CarSalesSystem.Data;
 using CarSalesSystem.Data.Model;
+using CarSalesSystem.DTOs;
+
 namespace CarSalesSystem.Controllers
 {
     public class CarController : Controller
@@ -13,54 +15,121 @@ namespace CarSalesSystem.Controllers
             _context = context;
         }
 
-    
+        [HttpGet]
         public IActionResult GetAll()
         {
-            var cars = _context.Cars.Include(c => c.Category).Include(c => c.Dealer).ToList();
+            var cars = _context.Cars
+                .Include(c => c.Category)
+                .Include(c => c.Dealer)
+                .Select(c => new CarDto
+                {
+                    Id = c.Id,
+                    Brand = c.Brand,
+                    Model = c.Model,
+                    Price = c.Price,
+                    ImageUrl = c.ImageUrl,
+                    City = c.City
+                })
+                .ToList();
+
             return View(cars);
         }
 
-    
+        [HttpGet]
         public IActionResult Add()
         {
-            return View();
+            return View(new CarFormDto());
         }
 
-      
+        
         [HttpPost]
-        public IActionResult Add(Car car)
+        public IActionResult Add(CarFormDto dto)
         {
             if (!ModelState.IsValid)
-                return View(car);
+                return View(dto);
+
+            var car = new Car
+            {
+                Brand = dto.Brand,
+                Model = dto.Model,
+                Description = dto.Description,
+                ImageUrl = dto.ImageUrl,
+                Year = dto.Year,
+                Mileage = dto.Mileage,
+                FuelType = dto.FuelType,
+                Transmission = dto.Transmission,
+                Price = dto.Price,
+                Country = dto.Country,
+                City = dto.City,
+                CategoryId = dto.CategoryId,
+                DealerId = dto.DealerId,
+                IsListed = true
+            };
 
             _context.Cars.Add(car);
             _context.SaveChanges();
-            return RedirectToAction("GetAll");
+
+            return RedirectToAction(nameof(GetAll));
         }
 
-
+        [HttpGet]
         public IActionResult Edit(int id)
         {
             var car = _context.Cars.Find(id);
             if (car == null)
                 return NotFound();
 
-            return View(car);
+            var dto = new CarFormDto
+            {
+                Brand = car.Brand,
+                Model = car.Model,
+                Description = car.Description,
+                ImageUrl = car.ImageUrl,
+                Year = car.Year,
+                Mileage = car.Mileage,
+                FuelType = car.FuelType,
+                Transmission = car.Transmission,
+                Price = car.Price,
+                Country = car.Country,
+                City = car.City,
+                CategoryId = car.CategoryId,
+                DealerId = car.DealerId
+            };
+
+            return View(dto);
         }
 
         
         [HttpPost]
-        public IActionResult Edit(Car car)
+        public IActionResult Edit(int id, CarFormDto dto)
         {
             if (!ModelState.IsValid)
-                return View(car);
+                return View(dto);
 
-            _context.Cars.Update(car);
+            var car = _context.Cars.Find(id);
+            if (car == null)
+                return NotFound();
+
+            car.Brand = dto.Brand;
+            car.Model = dto.Model;
+            car.Description = dto.Description;
+            car.ImageUrl = dto.ImageUrl;
+            car.Year = dto.Year;
+            car.Mileage = dto.Mileage;
+            car.FuelType = dto.FuelType;
+            car.Transmission = dto.Transmission;
+            car.Price = dto.Price;
+            car.Country = dto.Country;
+            car.City = dto.City;
+            car.CategoryId = dto.CategoryId;
+            car.DealerId = dto.DealerId;
+
             _context.SaveChanges();
-            return RedirectToAction("GetAll");
+
+            return RedirectToAction(nameof(GetAll));
         }
 
-        
+        [HttpGet]
         public IActionResult Details(int id)
         {
             var car = _context.Cars
@@ -71,9 +140,10 @@ namespace CarSalesSystem.Controllers
             if (car == null)
                 return NotFound();
 
-            return View(car);
+            return View(car); 
         }
 
+        
         [HttpPost]
         public IActionResult Delete(int id)
         {
@@ -83,27 +153,63 @@ namespace CarSalesSystem.Controllers
 
             _context.Cars.Remove(car);
             _context.SaveChanges();
-            return RedirectToAction("GetAll");
+
+            return RedirectToAction(nameof(GetAll));
         }
 
+        [HttpGet]
         public IActionResult SortByName()
         {
-            var cars = _context.Cars.OrderBy(c => c.Brand).ThenBy(c => c.Model).ToList();
+            var cars = _context.Cars
+                .OrderBy(c => c.Brand)
+                .ThenBy(c => c.Model)
+                .Select(c => new CarDto
+                {
+                    Id = c.Id,
+                    Brand = c.Brand,
+                    Model = c.Model,
+                    Price = c.Price,
+                    ImageUrl = c.ImageUrl,
+                    City = c.City
+                })
+                .ToList();
+
             return View("GetAll", cars);
         }
 
-
+        [HttpGet]
         public IActionResult SortByPrice()
         {
-            var cars = _context.Cars.OrderBy(c => c.Price).ToList();
+            var cars = _context.Cars
+                .OrderBy(c => c.Price)
+                .Select(c => new CarDto
+                {
+                    Id = c.Id,
+                    Brand = c.Brand,
+                    Model = c.Model,
+                    Price = c.Price,
+                    ImageUrl = c.ImageUrl,
+                    City = c.City
+                })
+                .ToList();
+
             return View("GetAll", cars);
         }
 
+        [HttpGet]
         public IActionResult CarsByCategory(int categoryId)
         {
             var cars = _context.Cars
                 .Where(c => c.CategoryId == categoryId)
-                .Include(c => c.Category)
+                .Select(c => new CarDto
+                {
+                    Id = c.Id,
+                    Brand = c.Brand,
+                    Model = c.Model,
+                    Price = c.Price,
+                    ImageUrl = c.ImageUrl,
+                    City = c.City
+                })
                 .ToList();
 
             return View("GetAll", cars);
--- CarSalesSystem/obj/Debug/net8.0/CarSalesSystem.AssemblyInfo.cs
+++ CarSalesSystem/obj/Debug/net8.0/CarSalesSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarSalesSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ead32b3f8c881b2cc80f49f651600d2460d9ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd83192acb26a83aef357cefe828ae5a8f421772")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarSalesSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarSalesSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
