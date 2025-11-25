0f88b5cf8f18c903565ae1d4d5dac0f88e7068ba
Sigma98 <alsalilee@gmail.com>
2025-11-25 11:50:17 +0200

    Debit card dto created

--- CarSalesSystem/DTOs/DebitCardDto.cs
+++ CarSalesSystem/DTOs/DebitCardDto.cs
@@ -2,5 +2,18 @@
 {
     public class DebitCardDto
     {
+        public string CardNumber { get; set; } = null!;
+
+   
+        public int CVV { get; set; }
+
+      
+        public string FullName { get; set; } = null!;
+
+        
+        public string ExpirationMonth { get; set; } = null!;
+
+        
+        public int ExpirationYear { get; set; }
     }
 }
