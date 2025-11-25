620ec00a9b52587de8aede7acb169482d6414f56
Sigma98 <alsalilee@gmail.com>
2025-11-25 07:43:55 +0200

    Updated Dealer entity

--- CarSalesSystem/Data/Model/Dealer.cs
+++ CarSalesSystem/Data/Model/Dealer.cs
@@ -4,31 +4,22 @@ namespace CarSalesSystem.Data.Model
 {
     public class Dealer
     {
-        /// <summary>
-        /// dealer id 
-        /// </summary>
         [Key]
         public int Id { get; set; }
 
-        /// <summary>
-        /// first name of the dealer
-        /// </summary>
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
 
-        /// <summary>
-        /// phone number of the dealer 
-        /// </summary>
+        public string? CompanyName { get; set; } 
+
         [Required]
         [Phone]
-        public string PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; } = null!;
 
-        /// <summary>
-        /// id of the user that is the dealer 
-        /// </summary>
         [Required]
-        public string UserId { get; set; }
+        public string UserId { get; set; } = null!;
 
-        public IEnumerable<Car>? Cars { get; init; } = new List<Car>();
+        public IEnumerable<Car> Cars { get; set; } = new List<Car>();
     }
+
 }
