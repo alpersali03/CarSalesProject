dadd27da6fd6e61a24e5c896c06e86f4c6853c18
Sigma98 <alsalilee@gmail.com>
2025-11-25 14:01:12 +0200

    Dealer controller updated

--- CarSalesSystem/obj/Debug/net8.0/CarSalesSystem.AssemblyInfo.cs
+++ CarSalesSystem/obj/Debug/net8.0/CarSalesSystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarSalesSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10709bdd21f999cbe702314b5fe09433cdf6ac5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff2b8e050fefbe35755e20c99aa5fae3bb5cd267")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarSalesSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarSalesSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
