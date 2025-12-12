a92670aa2880489c7642b113fd6f07005cc76a8d
Sigma98 <alsalilee@gmail.com>
2025-12-12 17:15:23 +0200

    Seed initial categories in database
    Added seeding of default car categories (Sedan, SUV, Pickup truck, Hatchback, Minivan, Sports car) in the ApplicationDbContext and created a corresponding migration to insert and remove these records. This ensures the Categories table is pre-populated with standard values.

--- CarSalesSystem/Data/ApplicationDbContext.cs
+++ CarSalesSystem/Data/ApplicationDbContext.cs
@@ -4,17 +4,32 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CarSalesSystem.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
-        public DbSet<Car> Cars { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Dealer> Dealers { get; set; }
-        public DbSet<DebitCard> DebitCards { get; set; }
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+		public DbSet<Car> Cars { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Dealer> Dealers { get; set; }
+		public DbSet<DebitCard> DebitCards { get; set; }
+		public DbSet<Payment> Payments { get; set; }
 
-        public DbSet<Payment> Payments { get; set; }
-    }
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.Entity<Category>().HasData
+				(
+				new Category { Id = 1, Name = "Sedan" },
+				new Category { Id = 2, Name = "SUV" },
+				new Category { Id = 3, Name = "Pickup truck" },
+				new Category { Id = 4, Name = "Hatchback" },
+				new Category { Id = 5, Name = "Minivan" },
+				new Category { Id = 6, Name = "Sports car" }
+
+				);
+		}
+	}
 }
