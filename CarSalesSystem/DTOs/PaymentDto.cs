ba4ec20a43c8531ec3a5c6a395c6ee699a10d074
Sigma98 <alsalilee@gmail.com>
2026-03-11 09:43:05 +0200

    Add DealerId to CarDto; expand PaymentDto
    Add DealerId property to CarDto and normalize whitespace/formatting. Expand PaymentDto to include payment metadata and card fields (CarId, CardNumber, CVV, FullName, ExpirationMonth, ExpirationYear) to support capturing payment details. Also includes an updated build artifact (obj/Debug/net8.0/apphost.exe).

--- CarSalesSystem/DTOs/PaymentDto.cs
+++ CarSalesSystem/DTOs/PaymentDto.cs
@@ -1,9 +1,17 @@
 ﻿namespace CarSalesSystem.DTOs
 {
-    public class PaymentDto
-    {
-        public DateTime PaymentTime { get; set; }
-        public decimal TotalAmount { get; set; }
-        public bool IsSuccessful { get; set; }
-    }
+	public class PaymentDto
+	{
+		public DateTime PaymentTime { get; set; }
+		public decimal TotalAmount { get; set; }
+		public bool IsSuccessful { get; set; }
+
+		public int CarId { get; set; }
+
+		public string CardNumber { get; set; } = null!;
+		public int CVV { get; set; }
+		public string FullName { get; set; } = null!;
+		public string ExpirationMonth { get; set; } = null!;
+		public int ExpirationYear { get; set; }
+	}
 }
